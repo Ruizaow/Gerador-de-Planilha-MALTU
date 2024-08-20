43ab900f107e08138b05e4df8eb9268ca40fd87f
Rui Fernandes <ruiwiebbeling@gmail.com>
2024-08-20 13:39:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
-# Faça as seguintes instalações no Terminal do programa para evitar possíveis erros:
-#   pip install pandas
-#   pip install google-play-scraper
-#   pip install openpyxl
-
 import pandas as pd
 from google_play_scraper import Sort, reviews
 
 # Coleta das avaliações
 result, continuation_token = reviews(
-    'com.google.android.apps.googleassistant',  # ID do aplicativo
-    lang='pt_BR',  # Linguagem
-    sort=Sort.MOST_RELEVANT,  # Mais relevantes
-    count=50,  # Quantidade a ser coletada
-    filter_score_with=None  # Coleta todas as avaliações
+    'com.google.android.apps.googleassistant',    # ID do aplicativo - modifique para o link de página quedeseja avaliar
+    lang='pt_BR',                                 # Linguagem
+    sort=Sort.MOST_RELEVANT,                      # Mais relevantes
+    count=50,                                     # Quantidade a ser coletada
+    filter_score_with=None                        # Coleta todas as avaliações
 )
 
 # Cria um DataFrame com as avaliações
